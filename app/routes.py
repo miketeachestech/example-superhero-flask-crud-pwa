e891eba176e798a949d9b7966df5675178872e56
Michael (Mike Teaches Tech) <miketeachestech@outlook.com.au>
2025-03-02 17:59:08 +1100

    Created form that allows users to add their own superheroes.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from app.database import get_db_connection
 
 main = Blueprint('main', __name__)
@@ -9,7 +9,7 @@ def home():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # Get all superheroes
+    # Get all superheroes (including images)
     cursor.execute("SELECT * FROM superheroes")
     superheroes = cursor.fetchall()
 
@@ -32,3 +32,23 @@ def home():
 def about():
     """Simple About page"""
     return render_template("about.html")
+
+@main.route('/add', methods=['GET', 'POST'])
+def add_superhero():
+    """Form to add a new superhero"""
+    if request.method == 'POST':
+        name = request.form['name']
+        alias = request.form['alias']
+        universe = request.form['universe']
+        image_url = request.form['image_url'] if request.form['image_url'] else 'default.png'
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO superheroes (name, alias, universe, image_url) VALUES (?, ?, ?, ?)",
+                       (name, alias, universe, image_url))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('main.home'))
+
+    return render_template("add_superhero.html")
