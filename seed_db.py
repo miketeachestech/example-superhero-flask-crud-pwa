30f899f0d119c398fd299da12f3b547b8ca04c7a
Michael (Mike Teaches Tech) <miketeachestech@outlook.com.au>
2025-03-02 20:43:42 +1100

    Added some more superheroes to seed data and polished the about page.

--- seed_db.py
+++ seed_db.py
@@ -11,6 +11,18 @@ def seed_data():
 
     # Insert Superheroes with Image URLs
     cursor.execute("INSERT INTO superheroes (name, alias, universe, image_url) VALUES (?, ?, ?, ?)", 
+               ("Peter Parker", "Spider-Man", "Marvel", "spiderman.png"))
+    spiderman_id = cursor.lastrowid
+
+    cursor.execute("INSERT INTO superheroes (name, alias, universe, image_url) VALUES (?, ?, ?, ?)", 
+                ("Diana Prince", "Wonder Woman", "DC", "wonderwoman.png"))
+    wonderwoman_id = cursor.lastrowid
+
+    cursor.execute("INSERT INTO superheroes (name, alias, universe, image_url) VALUES (?, ?, ?, ?)", 
+                ("T'Challa", "Black Panther", "Marvel", "blackpanther.png"))
+    blackpanther_id = cursor.lastrowid
+
+    cursor.execute("INSERT INTO superheroes (name, alias, universe, image_url) VALUES (?, ?, ?, ?)", 
                    ("Tony Stark", "Iron Man", "Marvel", "ironman.png"))
     ironman_id = cursor.lastrowid
 
@@ -28,7 +40,14 @@ def seed_data():
         ("Powered armor suit", ironman_id),
         ("Super strength", superman_id),
         ("Flight", superman_id),
-        ("Lots of money", batman_id)
+        ("Lots of money", batman_id),
+        ("Wall-crawling", spiderman_id),
+        ("Spider-Sense", spiderman_id),
+        ("Combat mastery", wonderwoman_id),
+        ("Immortality", wonderwoman_id),
+        ("Lasso of Truth", wonderwoman_id),
+        ("Superhuman strength", blackpanther_id),
+        ("Vibranium suit", blackpanther_id)
     ])
 
     conn.commit()
